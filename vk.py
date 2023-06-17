eb7ab9245f7004272cb3ea05f3b06b824f73759e
xworu <125150405+xworu@users.noreply.github.com>
2023-06-17 22:32:48 +0300

    Update vk.py
    Added connection to database with vk_id to mailing the everyday horoscope and it generation.

--- vk.py
+++ vk.py
@@ -1,7 +1,18 @@
+from main import generate_horoscope
 from social_spam import Vkontakte
 import schedule
+from datetime import date
 import time
 import os
+import psycopg2
+
+conn = psycopg2.connect(database="stars",
+                        user="postgres",
+                        password=" ",
+                        host="localhost",
+                        port="5432")
+
+cursor = conn.cursor()
 
 user_token = os.getenv("VK_TOKEN")
 # a personal token of your account with access to messages, friends and access at any time.
@@ -11,14 +22,25 @@ vk.connect_user(token=user_token)
 # The token can be obtained from the link: https://vkhost.github.io/
 
 
-def everyday_horoscope():
-    name = vk.get_name_by_id(434344214)['first_name']
-    vk.send_message(434344214, message=f'Hi, {name}! Horoscope advises you...', image='./image.png')
+def everyday_horoscope(records):
+    id = records[0][6]
+    current_date = date.today()
+    name = vk.get_name_by_id(id)['first_name']
+    horoscope = generate_horoscope()
+    vk.send_message(id, message=f'Доброго утра, {name}! {current_date} {horoscope}', image='./image.png')
     print("Message was sent successfully.")
 
 
-schedule.every().day.at("07:00").do(everyday_horoscope)
+def vk_sending():
+    cursor.execute("SELECT * FROM user_info WHERE vk_id is not null")
+    records = list(cursor.fetchall())
+    for i in records:
+        everyday_horoscope(records)
+
+
+schedule.every().day.at("07:00").do(vk_sending)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
